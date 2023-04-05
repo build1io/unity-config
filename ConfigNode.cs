846d2ee76a89127169ce3e86b514096bd56852b7
George Markosian <georgemarkosian@gmail.com>
2023-04-05 18:44:21 +0200

    Json serialization optimization.

--- ConfigNode.cs
+++ ConfigNode.cs
@@ -5,24 +5,23 @@ namespace Build1.UnityConfig
 {
     public abstract class ConfigNode
     {
+        private static readonly JsonSerializerSettings Settings = new()
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        };
+        
         public string ToJson(bool indented)
         {
-            var settings = new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                DefaultValueHandling = DefaultValueHandling.Ignore
-            };
-            
             if (indented)
-                return JsonConvert.SerializeObject(this, Formatting.Indented, settings);
-            
-            return JsonConvert.SerializeObject(this, settings);
+                return JsonConvert.SerializeObject(this, Formatting.Indented, Settings);
+            return JsonConvert.SerializeObject(this, Settings);
         }
-        
+
         /*
          * Static.
          */
-        
+
         public static ConfigNode FromJson(string json, Type type)
         {
             return (ConfigNode)JsonConvert.DeserializeObject(json, type);
