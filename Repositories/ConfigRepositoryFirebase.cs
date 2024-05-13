26cf428f55ab0d57928f50dea7da3c68553c233a
George Markosian <georgemarkosian@gmail.com>
2024-05-14 00:30:37 +0200

    Decomposed config parsing optimization.

--- Repositories/ConfigRepositoryFirebase.cs
+++ Repositories/ConfigRepositoryFirebase.cs
@@ -2,11 +2,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using Build1.UnityConfig.Utils;
 using Firebase.Extensions;
 using Firebase.RemoteConfig;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using UnityEngine;
 
 namespace Build1.UnityConfig.Repositories
@@ -15,35 +17,15 @@ namespace Build1.UnityConfig.Repositories
     {
         private static readonly Regex BooleanTruePattern  = new("^(1|true|t|yes|y|on)$", RegexOptions.IgnoreCase);
         private static readonly Regex BooleanFalsePattern = new("^(0|false|f|no|n|off|)$", RegexOptions.IgnoreCase);
-        
+
         public static void Load(ConfigSettings settings, Type configType, Action<ConfigNode> onComplete, Action<ConfigException> onError)
         {
-            SetConfigSettings(settings, () =>
-            {
-                FetchConfig(() =>
-                {
-                    GetJson(settings, json =>
-                    {
-                        var config = (ConfigNode)JsonConvert.DeserializeObject(json, configType);
-                        onComplete?.Invoke(config);
-                    }, onError);
-                }, onError);
-            }, onError);
+            SetConfigSettings(settings, () => { FetchConfig(() => { GetJsonAndParse(settings, configType, onComplete, onError); }, onError); }, onError);
         }
 
         public static void Load<T>(ConfigSettings settings, Action<T> onComplete, Action<ConfigException> onError) where T : ConfigNode
         {
-            SetConfigSettings(settings, () =>
-            {
-                FetchConfig(() =>
-                {
-                    GetJson(settings, json =>
-                    {
-                        var config = JsonConvert.DeserializeObject<T>(json);
-                        onComplete?.Invoke(config);
-                    }, onError);
-                }, onError);
-            }, onError);
+            SetConfigSettings(settings, () => { FetchConfig(() => { GetJsonAndParse(settings, onComplete, onError); }, onError); }, onError);
         }
 
         /*
@@ -57,8 +39,8 @@ namespace Build1.UnityConfig.Repositories
                 var configSettings = new global::Firebase.RemoteConfig.ConfigSettings();
 
                 if (settings is { FallbackEnabled: true, FallbackTimeout: > 0 })
-                    configSettings.FetchTimeoutInMilliseconds = (ulong)settings.FallbackTimeout;    
-                
+                    configSettings.FetchTimeoutInMilliseconds = (ulong)settings.FallbackTimeout;
+
                 if (Debug.isDebugBuild)
                     configSettings.MinimumFetchInternalInMilliseconds = 0; // Refresh immediately when debugging.
 
@@ -104,15 +86,46 @@ namespace Build1.UnityConfig.Repositories
             }
         }
 
-        private static void GetJson(ConfigSettings settings, Action<string> onComplete, Action<ConfigException> onError)
+        private static void GetJsonAndParse(ConfigSettings settings, Type configType, Action<ConfigNode> onComplete, Action<ConfigException> onError)
         {
             switch (settings.Mode)
             {
                 case ConfigMode.Default:
-                    GetJsonDefault(settings, onComplete, onError);
+                    GetJsonDefault(settings, json =>
+                    {
+                        var config = (ConfigNode)JsonConvert.DeserializeObject(json, configType);
+                        onComplete?.Invoke(config);
+                    }, onError);
                     break;
                 case ConfigMode.Decomposed:
-                    GetJsonDecomposed(settings, onComplete, onError);
+                    GetDecomposed(settings, values =>
+                    {
+                        var instance = ParseDecomposed(values, configType);
+                        onComplete?.Invoke(instance);
+                    }, onError);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private static void GetJsonAndParse<T>(ConfigSettings settings, Action<T> onComplete, Action<ConfigException> onError) where T : ConfigNode
+        {
+            switch (settings.Mode)
+            {
+                case ConfigMode.Default:
+                    GetJsonDefault(settings, json =>
+                    {
+                        var config = JsonConvert.DeserializeObject<T>(json);
+                        onComplete?.Invoke(config);
+                    }, onError);
+                    break;
+                case ConfigMode.Decomposed:
+                    GetDecomposed(settings, values =>
+                    {
+                        var instance = ParseDecomposed<T>(values);
+                        onComplete?.Invoke(instance);
+                    }, onError);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
@@ -124,7 +137,7 @@ namespace Build1.UnityConfig.Repositories
             try
             {
                 var field = settings.ParameterName;
-        
+
                 if (!FirebaseRemoteConfig.DefaultInstance.AllValues.ContainsKey(field))
                 {
                     onError?.Invoke(new ConfigException(ConfigError.ConfigFieldNotFound, $"Parameter \"{field}\" not found in Firebase Remote Config."));
@@ -145,29 +158,29 @@ namespace Build1.UnityConfig.Repositories
                     onError?.Invoke(exception.ToConfigException());
                     return;
                 }
-                        
+
                 onComplete?.Invoke(json);
             }
             catch (Exception exception)
             {
                 Debug.LogException(exception);
-                
+
                 onError?.Invoke(exception.ToConfigException());
             }
         }
 
-        private static void GetJsonDecomposed(ConfigSettings settings, Action<string> onComplete, Action<ConfigException> onError)
+        private static void GetDecomposed(ConfigSettings settings, Action<Dictionary<string, object>> onComplete, Action<ConfigException> onError)
         {
             try
             {
                 var parameters = new Dictionary<string, object>(FirebaseRemoteConfig.DefaultInstance.AllValues.Count);
-                
+
                 foreach (var pair in FirebaseRemoteConfig.DefaultInstance.AllValues)
                 {
                     var value = pair.Value.StringValue;
                     if (value.Length >= 24)
                     {
-                        parameters.Add(pair.Key, ParseAsString(value));
+                        parameters.Add(pair.Key, TryDecompress(value));
                     }
                     else if (value.Length > 0)
                     {
@@ -189,29 +202,84 @@ namespace Build1.UnityConfig.Repositories
                         }
                         else
                         {
-                            parameters.Add(pair.Key, ParseAsString(value));
+                            parameters.Add(pair.Key, TryDecompress(value));
                         }
                     }
                 }
 
-                var json = JsonConvert.SerializeObject(parameters);
-
-                onComplete?.Invoke(json);
+                onComplete?.Invoke(parameters);
             }
             catch (Exception exception)
             {
                 Debug.LogException(exception);
-                
+
                 onError?.Invoke(exception.ToConfigException());
             }
         }
 
-        private static object ParseAsString(string value)
+        private static object TryDecompress(string value)
         {
             if (!value.StartsWith("{") || !value.EndsWith("}"))
                 value = value.Decompress();
-                        
-            return JsonConvert.DeserializeObject(value);
+            return value;
+        }
+
+        private static ConfigNode ParseDecomposed(IDictionary<string, object> values, Type configType)
+        {
+            var instance   = (ConfigNode)Activator.CreateInstance(configType);
+            var properties = instance.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var jsonPropertyAttribute = property.GetCustomAttribute<JsonPropertyAttribute>();
+                if (jsonPropertyAttribute?.PropertyName == null)
+                    continue;
+
+                if (!values.TryGetValue(jsonPropertyAttribute.PropertyName, out var value))
+                    continue;
+
+                if (value is string json)
+                {
+                    var propertyInstance = JsonConvert.DeserializeObject(json, property.PropertyType);
+                    property.SetValue(instance, propertyInstance);
+                }
+                else
+                {
+                    property.SetValue(instance, value);
+                }
+            }
+
+            return instance;
+        }
+
+        private static T ParseDecomposed<T>(IDictionary<string, object> values)
+        {
+            var instance   = Activator.CreateInstance<T>();
+            var properties = instance.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var jsonPropertyAttribute = property.GetCustomAttribute<JsonPropertyAttribute>();
+                if (jsonPropertyAttribute?.PropertyName == null)
+                    continue;
+
+                if (!values.TryGetValue(jsonPropertyAttribute.PropertyName, out var value))
+                    continue;
+
+                Debug.Log(jsonPropertyAttribute.PropertyName + ": " + value);
+                
+                if (value is string json)
+                {
+                    var propertyInstance = JsonConvert.DeserializeObject(json, property.PropertyType);
+                    property.SetValue(instance, propertyInstance);
+                }
+                else
+                {
+                    property.SetValue(instance, value);
+                }
+            }
+
+            return instance;
         }
     }
 }
